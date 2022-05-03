00efc95fd9ab71cf0a5b77432bdbeed6110f7b5b
Anubis-1001 <104244595+Anubis-1001@users.noreply.github.com>
2022-05-03 17:57:17 -0500

    Solo tiene los métodos definidos no implementados

---
+++ src/ControladorMesa.java
@@ -0,0 +1,27 @@
+package vista;
+
+import model.Ficha;
+import model.Jugador;
+
+public class ControladorMesa {
+	
+	public void inicializar()
+	{
+		
+	}
+	
+	public void colocarFicha(Ficha ficha)
+	{
+		
+	}
+	
+	public void mostrarFichasJugador(Jugador jugador)
+	{
+		
+	}
+	
+	public void renderizar()
+	{
+		
+	}
+}
